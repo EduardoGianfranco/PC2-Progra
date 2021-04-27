9d0b7ecaa14f89c3441ea05756c95867671bbafc
Eduardo Bustios <eduardo_bustios@usmp.pe>
2021-04-27 18:51:26 -0500

    Creados Clase usuario y editada la pagina principal

---
+++ Controllers/RegistroController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PC2_Progra.Controllers
+{
+    public class RegistroController : Controller
+    {
+        
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,11 @@
+namespace PC2_Progra.Models
+{
+    public class Usuario
+    {
+        public int id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Contraseña { get; set; }
+        public string Correo { get; set; }
+    }
+}
