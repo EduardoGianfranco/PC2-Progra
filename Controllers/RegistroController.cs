70305644fa62a30bff720f5798c33f4ad1989f52
Eduardo Bustios <eduardo_bustios@usmp.pe>
2021-04-27 19:25:27 -0500

    Creados Controller y redireccionado el registro.

--- Controllers/RegistroController.cs
+++ Controllers/RegistroController.cs
@@ -1,9 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
+using PC2_Progra.Models;
 
 namespace PC2_Progra.Controllers
 {
     public class RegistroController : Controller
     {
-        
+        public IActionResult Registro(){
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registro(Usuario u){
+            if (ModelState.IsValid){
+                return RedirectToAction("Actualizar");
+            }
+            return View();
+        }
+        public IActionResult Actualizar(){
+            return View();
+        }
     }
 }
